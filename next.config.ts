f47cea410434516e0afb213e6758f44b3edfc665
gauthamksn <gauthamkrishnax@gmail.com>
2025-12-31 13:22:46 +0530

    change config to proxy

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
 import type { NextConfig } from "next";
-import { domainMappings } from "./config/domains";
-
-// Generate rewrites from domain mappings
-const domainRewrites = Object.values(domainMappings).flatMap((mapping) => [
-  {
-    source: "/",
-    has: [{ type: "host" as const, value: mapping.domain }],
-    destination: mapping.path,
-  },
-  {
-    source: "/:path+",
-    has: [{ type: "host" as const, value: mapping.domain }],
-    destination: `${mapping.path}/:path+`,
-  },
-]);
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-
-  async rewrites() {
-    return {
-      beforeFiles: domainRewrites,
-    };
-  },
 };
 
 export default nextConfig;
